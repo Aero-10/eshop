6b569091b85546bb0e52c38f60e8437528310b1e
Aero-10 <prakharsinghal999@gmail.com>
2025-05-31 14:54:33 +0530

    commit before changing to frontend branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
-function App() {
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
 
+function App() {
   return (
-    <main>
-      <Outlet/>
-    </main>
+    <>
+      <Header/>
+      <main>
+        <Outlet/>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const Home = () => {
+const Header = () => {
   return (
     <div>
-      jjjjjh
+      header
     </div>
   )
 }
 
-export default Home
+export default Header
